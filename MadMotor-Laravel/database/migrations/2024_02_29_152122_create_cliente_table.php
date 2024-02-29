59c98ef5d7de659d0b74883b7353c81d707c505d
JBrandonCL <23jbcl@gmail.com>
2024-02-29 16:23:43 +0100

    Cliente Migrate

---
+++ MadMotor-Laravel/database/migrations/2024_02_29_152122_create_cliente_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cliente', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('email');
+            $table->string('password');
+            $table->string('role')->default('cliente');
+            $table->string('apellido');
+            $table->string('dni')->unique();
+            $table->string('imagen')->default('https://via.placeholder.com/150');
+            $table->boolean('isDeleted')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cliente');
+    }
+};
