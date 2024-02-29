c63c72b92485dc4644a633f53169ae6f11a98944
JBrandonCL <23jbcl@gmail.com>
2024-02-29 16:42:56 +0100

    Add seeder

---
+++ MadMotor-Laravel/database/seeders/ClientesSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ClientesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('clientes')->insert([[
+            'id' => 1,
+            'nombre' => 'Paco',
+            'email'=>'paco2@Paco1234.com',
+            'password' => '$2y$12$Su01nVy36tGMGbFjBhk4HugNrj9MoasmS0IUO5/mzuaq4WwrIkC2q',
+            'role' => 'cliente',
+            'apellido' => 'Paco',
+            'dni' => '12345678X',
+            'imagen' => 'https://via.placeholder.com/150',
+            'isDeleted' => false,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]]);
+    }
+}
