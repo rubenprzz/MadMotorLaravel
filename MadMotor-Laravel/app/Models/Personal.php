ab179728f803fe66c67719fcf0e5ddb2ee879ad6
rubenprzz <rubenfernandezp1703@gmail.com>
2024-02-29 18:27:04 +0100

    Modelo Pieza

--- MadMotor-Laravel/app/Models/Personal.php
+++ MadMotor-Laravel/app/Models/Personal.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
-class Personal extends Model {
+class   Personal extends Model {
     protected $table = 'personal';
     protected $fillable = [
         'nombre',
