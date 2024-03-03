adebd8457f307d8246765e1f713ac8b8707822e5
JBrandonCL <23jbcl@gmail.com>
2024-03-03 13:18:47 +0100

    Redirect

--- MadMotor-Laravel/app/Providers/RouteServiceProvider.php
+++ MadMotor-Laravel/app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/welcome';
+    public const HOME = '/home';
     public const PERSONAL_HOME = '/personal.home';
 
     /**
